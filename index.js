58f65c904af79475d623ec1d71f1e081fc70952a
Hammouda Ben Mekki <hbmekki@gmail.com>
2024-05-17 18:55:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const url = require('url').URL;
+const bodyParser = require('body-parser');
+
+const urlMap = [];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,8 +22,25 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+
+  try {
+    new url(req.body.url);
+    const urlItem ={
+      original_url : req.body.url,  short_url : urlMap.length + 1
+    };
+    urlMap.push(urlItem)
+
+    return  res.json(urlItem);
+  } catch(err) {
+    res.json({ error: 'invalid url' });
+  }
+});
+
+app.get('/api/shorturl/:short', function(req, res) {
+  const short = Number(req.params.short);
+  const url = urlMap[short - 1]["original_url"];
+  res.redirect(url);
 });
 
 app.listen(port, function() {
